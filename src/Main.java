5239ad5965f0b66893fd7fcab1564d416afade75
xiaoming <liuxiaomingskm@gmail.com>
2020-03-02 11:13:00 -0800

    "Second commit"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[]args){
+
+
+    public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        List<String> strList = new ArrayList<String>(10);
+
     }
 }
